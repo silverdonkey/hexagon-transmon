5671fd3fd9f23a830eb64e5ef13c76aa4f2a2488
Nikolay Orozov <dev@niko-consulting.de>
2023-09-08 14:57:15 +0200

    organizing code: created an architecturally expressive package structure

--- src/main/java/de/nikoconsulting/demo/hexagontransmon/HexagonTransmonApplication.java
+++ src/main/java/de/nikoconsulting/demo/hexagontransmon/HexagonTransmonApplication.java
@@ -1,4 +1,4 @@
-package de.nikoconsulting.demo;
+package de.nikoconsulting.demo.hexagontransmon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/de/nikoconsulting/demo/hexagontransmon/HexagonTransmonApplicationTests.java
+++ src/test/java/de/nikoconsulting/demo/hexagontransmon/HexagonTransmonApplicationTests.java
@@ -1,4 +1,4 @@
-package de.nikoconsulting.demo;
+package de.nikoconsulting.demo.hexagontransmon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
