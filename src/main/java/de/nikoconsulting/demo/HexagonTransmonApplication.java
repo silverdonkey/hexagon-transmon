0153315975f11e1ff37c13cf193dbdff60fd279a
Nikolay Orozov <dev@niko-consulting.de>
2023-09-08 14:27:56 +0200

    init: generated by spring-boot starter

---
+++ src/main/java/de/nikoconsulting/demo/HexagonTransmonApplication.java
@@ -0,0 +1,13 @@
+package de.nikoconsulting.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonTransmonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonTransmonApplication.class, args);
+	}
+
+}
