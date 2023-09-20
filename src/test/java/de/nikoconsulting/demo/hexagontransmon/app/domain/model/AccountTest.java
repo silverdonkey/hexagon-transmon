447056f618edaf211ae073cdb196c31f7021270a
Nikolay Orozov <dev@niko-consulting.de>
2023-09-20 10:28:14 +0200

    testing a domain entity with unit tests

---
+++ src/test/java/de/nikoconsulting/demo/hexagontransmon/app/domain/model/AccountTest.java
@@ -0,0 +1,103 @@
+package de.nikoconsulting.demo.hexagontransmon.app.domain.model;
+
+import org.junit.jupiter.api.Test;
+
+import de.nikoconsulting.demo.hexagontransmon.app.domain.model.Account.AccountId;
+
+import static de.nikoconsulting.demo.hexagontransmon.common.AccountTestData.defaultAccount;
+import static de.nikoconsulting.demo.hexagontransmon.common.ActivityTestData.defaultActivity;
+import static org.assertj.core.api.Assertions.*;
+
+
+public class AccountTest {
+
+    @Test
+    void withdrawalSucceeds() {
+        AccountId accountId = new AccountId(1L);
+        Account account = defaultAccount()
+                .withAccountId(accountId)
+                .withBaselineBalance(Money.of(555L))
+                .withActivityWindow(new ActivityWindow(
+                        defaultActivity()
+                                .withTargetAccount(accountId)
+                                .withMoney(Money.of(999L)).build(),
+                        defaultActivity()
+                                .withTargetAccount(accountId)
+                                .withMoney(Money.of(1L)).build()))
+                .build();
+
+        AccountId randomTargetAccount = new AccountId(99L);
+        boolean success = account.withdraw(Money.of(1000L), randomTargetAccount);
+
+        assertThat(success).isTrue();
+        assertThat(account.getActivityWindow().getActivities()).hasSize(3);
+        assertThat(account.calculateBalance()).isEqualTo(Money.of(555L));
+    }
+
+    @Test
+    void withdrawalFailure() {
+        AccountId accountId = new AccountId(1L);
+        Account account = defaultAccount()
+                .withAccountId(accountId)
+                .withBaselineBalance(Money.of(555L))
+                .withActivityWindow(new ActivityWindow(
+                        defaultActivity()
+                                .withTargetAccount(accountId)
+                                .withMoney(Money.of(999L)).build(),
+                        defaultActivity()
+                                .withTargetAccount(accountId)
+                                .withMoney(Money.of(1L)).build()))
+                .build();
+
+        // try to withdraw more money => this should fail
+        boolean success = account.withdraw(Money.of(1556L), new AccountId(99L));
+        // check that the result of withdrawal is false
+        assertThat(success).isFalse();
+        assertThat(account.getActivityWindow().getActivities()).hasSize(2);
+        assertThat(account.calculateBalance()).isEqualTo(Money.of(1555L));
+    }
+
+
+    @Test
+    void calculatesBalance() {
+        AccountId accountId = new AccountId(1L);
+        Account account = defaultAccount()
+                .withAccountId(accountId)
+                .withBaselineBalance(Money.of(500L))
+                .withActivityWindow(new ActivityWindow(
+                        defaultActivity()
+                                .withTargetAccount(accountId)
+                                .withMoney(Money.of(999L)).build(),
+                        defaultActivity()
+                                .withTargetAccount(accountId)
+                                .withMoney(Money.of(1L)).build()))
+                .build();
+
+        Money balance = account.calculateBalance();
+
+        assertThat(balance).isEqualTo(Money.of(1500L));
+    }
+
+    @Test
+    void depositSuccess() {
+        AccountId accountId = new AccountId(1L);
+        Account account = defaultAccount()
+                .withAccountId(accountId)
+                .withBaselineBalance(Money.of(555L))
+                .withActivityWindow(new ActivityWindow(
+                        defaultActivity()
+                                .withTargetAccount(accountId)
+                                .withMoney(Money.of(999L)).build(),
+                        defaultActivity()
+                                .withTargetAccount(accountId)
+                                .withMoney(Money.of(1L)).build()))
+                .build();
+
+        boolean success = account.deposit(Money.of(445L), new AccountId(99L));
+
+        assertThat(success).isTrue();
+        assertThat(account.getActivityWindow().getActivities()).hasSize(3);
+        assertThat(account.calculateBalance()).isEqualTo(Money.of(2000L));
+    }
+
+}
