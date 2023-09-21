9efa15c32cd715f50e50ad3e925d4bd0d71b6455
Nikolay Orozov <dev@niko-consulting.de>
2023-09-21 11:07:59 +0200

    configure the default SpringBootTest to use in-memory H2 db

--- src/test/java/de/nikoconsulting/demo/hexagontransmon/HexagonTransmonApplicationTests.java
+++ src/test/java/de/nikoconsulting/demo/hexagontransmon/HexagonTransmonApplicationTests.java
@@ -1,11 +1,24 @@
 package de.nikoconsulting.demo.hexagontransmon;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb_inmem",
+		"spring.datasource.driverClassName=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=password",
+		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+		"spring.jpa.hibernate.ddl-auto=create",
+		"spring.jpa.show-sql=true",
+		"spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
+		"spring.jpa.properties.hibernate.format_sql=true"
+})
 class HexagonTransmonApplicationTests {
 
-	//@Test
+	@Test
 	void contextLoads() {
 	}
 
